78b2622953f5804e3e5fd0ba2012daa75cdac766
Dennis Kumar <dennis@lieturd.com>
2019-07-27 13:02:42 +0300

    Faq page, Arrow added.

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -32,13 +32,12 @@ const StyledSection = styled.section`
     position: relative;
 `;
 const StyledQuestion = styled.div`
-
     margin: 0.5rem;
     max-height: 12rem;
     width: 24rem;
-    padding:1rem;
+    padding: 1rem;
     background: ${colors.mainDarker};
-    transition-duration: 0.3s;
+    transition-duration: 0.5s;
     border: 2px solid ${colors.mainLightest};
     text-align: left;
     cursor: pointer;
@@ -46,16 +45,17 @@ const StyledQuestion = styled.div`
     letter-spacing: 0.04em;
     word-spacing: 0.05em;
     line-height: 1.5em;
+    position: relative;
 
     &&&:hover {
-      transform: scale(1.05);
-      box-shadow: 0 0 5px 2px ${colors.mainLightest};
+        transform: scale(1.05);
+        box-shadow: 0 0 5px 2px ${colors.mainLightest};
     }
 
     &&&:active {
-      transform: scale(1);
-      box-shadow: 0 0 5px 2px ${colors.mainLightest};
-      transition-duration: 0.1s;
+        transform: scale(1);
+        box-shadow: 0 0 5px 2px ${colors.mainLightest};
+        transition-duration: 0.1s;
     }
 `;
 
@@ -75,34 +75,65 @@ const StyledHr = styled.hr`
     color: ${colors.mainLightest};
 `;
 
+const StyledArrow = styled.span`
+    height: 10px;
+    width: 10px;
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+    border-top: 2px solid ${colors.mainLightest};
+    border-right: 2px solid ${colors.mainLightest};
+    transform: rotate(135deg);
+    transition-duration: 0.5s;
+`;
+
 class Faq extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    handleTest = (event) => {
-
+    handleTest = event => {
         const target = event.target;
-        const child = target.querySelector('p');
-        const height = target.tagName === 'DIV' ? target.style.maxHeight : target.parentNode.style.maxHeight;
+        const child =
+            target.tagName === 'DIV'
+                ? target.querySelector('p')
+                : target.parentNode.querySelector('p');
+        const height =
+            target.tagName === 'DIV'
+                ? target.style.maxHeight
+                : target.parentNode.style.maxHeight;
 
-        if (height == '12rem') {
+        if (height === '12rem' || height === '') {
             child.style.display = 'block';
             child.style.opacity = '1';
             target.style.maxHeight = '50rem';
-        }   else {
-
-            target.tagName === 'P' || target.tagName === 'HR' ? target.style.display = 'none' : child.style.display  = 'none';
-
-            target.tagName === 'P' || target.tagName === 'HR' ? target.style.opacity = '0' : child.style.opacity = '0';
-
-            target.tagName === 'P' || target.tagName === 'HR' ? target.parentNode.style.maxHeight = '12rem' : target.style.maxHeight = '12rem';
+            target.tagName === 'DIV'
+                ? (target.querySelector('span').style.transform =
+                      'rotate(-45deg)')
+                : (target.style.transform = 'rotate(-45deg)');
+        } else {
+            if (
+                target.tagName === 'P' ||
+                target.tagName === 'HR' ||
+                target.tagName === 'SPAN'
+            ) {
+                target.style.display = 'none';
+                target.style.opacity = '0';
+                target.parentNode.style.maxHeight = '12rem';
+                target.parentNode.querySelector('span').style.transform =
+                    'rotate(135deg)';
+            } else {
+                child.style.opacity = '0';
+                child.style.display = 'none';
+                target.style.maxHeight = '12rem';
+                target.querySelector('span').style.transform = 'rotate(135deg)';
+            }
         }
     };
 
-    render(){
+    render() {
         const { edges: posts } = this.props.data.allMdx;
-        posts.filter(post => console.log(post.node.frontmatter.question));
+
         return (
             <Layout>
                 <SEO title="FAQ Platform of Trust" />
@@ -112,7 +143,14 @@ class Faq extends React.Component {
                     <StyledSection className="container">
                         {posts.map(post => {
                             return (
-                                <StyledQuestion onClick={this.handleTest}>{post.node.frontmatter.question}<StyledAnswer><StyledHr />{post.node.frontmatter.answer}</StyledAnswer></StyledQuestion>
+                                <StyledQuestion onClick={this.handleTest}>
+                                    {post.node.frontmatter.question}
+                                    <StyledAnswer>
+                                        <StyledHr />
+                                        {post.node.frontmatter.answer}
+                                    </StyledAnswer>
+                                    <StyledArrow />
+                                </StyledQuestion>
                             );
                         })}
                     </StyledSection>
@@ -120,30 +158,22 @@ class Faq extends React.Component {
             </Layout>
         );
     }
-
-
 }
-
-
-
 
 export default Faq;
 
 export const query = graphql`
-query faqQuery {
-    allMdx(
-        filter: { frontmatter: { type: { eq: "faq" } } }
-
-    ) {
-        edges {
-            node {
-                id
-                frontmatter {
-                    question
-                    answer
+    query faqQuery {
+        allMdx(filter: { frontmatter: { type: { eq: "faq" } } }) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        question
+                        answer
+                    }
                 }
             }
         }
     }
-}
 `;
