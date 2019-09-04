0d1ff0906835a74741168fd533e6589f41d0478b
Dennis Kumar <dennis@lieturd.com>
2019-09-04 10:25:22 +0300

    Removed strange behaviour of repeating properties

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const StyledToggler = styled.div`
     position: fixed;
     display: block;
     box-sizing: content-box;
-    // width: 2rem;width
+    // width: 2rem;
     top: 1.8rem;
     left: 0;
     transform: translateX(calc(100vw - 4.2rem)) rotate(0deg);
